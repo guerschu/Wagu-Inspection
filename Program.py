1b0dc9a0aef7d27ec1940a51f7aa9deca444dbbf
ziqiuf <ziqiuf04@gmail.com>
2024-04-06 01:52:14 -0600

    well

--- Program.py
+++ Program.py
@@ -16,51 +16,33 @@ data = pandas.read_csv('Data.csv')
 RibeyeMeasurements = data["Ribeye"]
 fatThickness = data.FatThickness
 
-x = torch.load('TrainImages')
-y = torch.load('TrainImages')
 class CTDataset(Dataset):
-    # def init(self, filepath):
-    #     self.x, self.y = torch.load(filepath)
-    #     for i in range(len(self.x)):  # Iterate over the indices of the tensor x
-    #         for j in range(len(self.x[i-1])):
-    #             print(self.x[i])
-    #         self.x[i] += self.x[i] / 255.  # Divide each element by 255
-    #     self.y = F.one_hot(self.y, num_classes=10).to(torch.float32)  # Convert y to one-hot and float32
+    def __init__(self, root_dir, transform=None):
+        self.root_dir = root_dir
+        self.transform = transform
+        self.images = self.load_images()
 
-        # Directory containing the TIFF images
-    def __init__(self, filepath):
+    def __len__(self):
+        return len(self.images)
 
-        tif_dir = filepath
+    def __getitem__(self, idx):
+        img_name = os.path.join(self.root_dir, self.images[idx])
+        image = Image.open(img_name)
+        if self.transform:
+            image = self.transform(image)
+        return image
 
-        # List to store loaded images
+    def load_images(self):
         images = []
-
-        # Iterate over TIFF files in the directory
-        for file in os.listdir(tif_dir):
+        for file in os.listdir(self.root_dir):
             if file.endswith('.tif'):
-                # Load the TIFF image using PIL
-                img = Image.open(os.path.join(tif_dir, file))
-                # Convert the image to a PyTorch tensor
-                img_tensor = transforms.ToTensor()(img)
-                # Append the tensor to the list of images
-                images.append(img_tensor)
-
-        # Stack the list of tensors into a single tensor
-        images_tensor = torch.stack(images)
-        print("Tensor shape:", images_tensor.shape)  # Print tensor shape (batch_size, channels, height, width)
-    def len(self):
-        return self.x.shape[0]
-    def getitem(self, ix):
-        return self.x[ix], self.y[ix]
-
-
-
-train_ds = CTDataset('TrainImages')
-
-test_ds = CTDataset('TestImages')
+                images.append(file)
+        return images
 
 transform = transforms.Compose([
     transforms.Grayscale(),
-    transforms.ToTensor(),           # Convert to PyTorch tensor
-    # Add more transformations as needed
+    transforms.ToTensor()
 ])
+
+train_ds = CTDataset(root_dir='TrainImages', transform=transform)
+test_ds = CTDataset(root_dir='TestImages', transform=transform)
